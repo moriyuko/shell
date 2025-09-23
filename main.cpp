b6cfe9e1e7718d23a1e6a5ebeef72b17ec718655
moriyuko <106382428+moriyuko@users.noreply.github.com>
2025-09-23 20:09:53 +0300

    history added

--- main.cpp
+++ main.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <cstdlib>
 
 int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   std::string input;
+  
+  std::string home = getenv("HOME");
+  std::string history_file = home + "/.kubsh_history";
+
+  std::ofstream history_out(history_file, std::ios::app);
+  if (!history_out.is_open()) {
+    std::cerr << "History file unavailable!" << std::endl;
+  }
+
   while (true) {
+
         std::cout << "kubsh$ ";
+
         if (!std::getline(std::cin, input)) {
             std::cout << "\nExiting...\n";
             break;
         }
+
+        if (history_out.is_open()) {
+            history_out << input << std::endl;
+        }
+
         if (input == "\\q") {
             std::cout << "Exiting...\n";
             break;
