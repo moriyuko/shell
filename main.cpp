53ac760aa8d0cd89ca5ea556d3b5d0de7e2256c2
moriyuko <106382428+moriyuko@users.noreply.github.com>
2025-09-26 10:32:18 +0300

    back 2

--- main.cpp
+++ main.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <sstream>
-#include <pwd.h>
-#include <unistd.h>
-
-std::string expandHome(const std::string& path) {
-    if (path.empty() || path[0] != '~') return path;
-    struct passwd *pw = getpwuid(getuid());
-    if (!pw) return path;
-    return std::string(pw->pw_dir) + path.substr(1);
-}
+#include <cstdlib>
 
 int main() {
   std::cout << std::unitbuf;
@@ -44,37 +35,11 @@ int main() {
             continue; //добавила, чтоб не было вывода ": command not found"
         }
 
-        if (input.rfind("\\e", 0) == 0) {
-            std::string var = input.substr(3); // всё после "\e "
-            if (!var.empty() && var[0] == '$') {
-                var = var.substr(1);
-                const char* val = getenv(var.c_str());
-                if (val) {
-                    std::string value = val;
-                    if (value.find(':') != std::string::npos) {
-                        std::stringstream ss(value);
-                        std::string token;
-                        while (std::getline(ss, token, ':')) {
-                            std::cout << token << std::endl;
-                        }
-                    } else {
-                        std::cout << value << std::endl;
-                    }
-                } else {
-                    std::cout << "Переменная не найдена" << std::endl;
-                }
-            } else {
-                std::cout << "Формат: \\e $PATH" << std::endl;
-            }
-            continue;
-        }
-
         if (input == "\\q") {
             std::cout << "Exiting...\n";
             break;
         }
         std::cout << input << ": command not found" << std::endl;
     }
-    if (history_out.is_open()) history_out.close();
     return 0;
 }
